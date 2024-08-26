42dc8892816cd2d1841f3fb5d89cf5e5eddc0c7d
Favour <ayofavour7@gmail.com>
2024-08-26 19:25:39 +0100

    fix: update assertion to silent pass

--- src/assertions.js
+++ src/assertions.js
@@ -5,55 +5,27 @@ const validate = (actual) => {
 
   return {
     isEqual: (expected) => {
-      if (JSON.stringify(actual) === JSON.stringify(expected)) {
-        return {
-          pass: true,
-          message: `Expected ${actual} to be ${expected}`,
-        };
-      } else {
-        return {
-          pass: false,
-          message: `Expected ${actual} to be ${expected}, but it was not.`,
-        };
+      if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+        throw new Error(
+          `Expected ${actual} to be ${expected}, but it was not.`
+        );
       }
     },
     isNotEqual: (expected) => {
-      if (JSON.stringify(actual) !== JSON.stringify(expected)) {
-        return {
-          pass: true,
-          message: `Expected ${actual} to not be ${expected}`,
-        };
-      } else {
-        return {
-          pass: false,
-          message: `Expected ${actual} to not be ${expected}, but it was.`,
-        };
+      if (JSON.stringify(actual) === JSON.stringify(expected)) {
+        throw new Error(
+          `Expected ${actual} to not be ${expected}, but it was.`
+        );
       }
     },
     isTruthy: () => {
-      if (actual) {
-        return {
-          pass: true,
-          message: `Expected ${actual} to be truthy`,
-        };
-      } else {
-        return {
-          pass: false,
-          message: `Expected ${actual} to be truthy, but it was falsy.`,
-        };
+      if (!actual) {
+        throw new Error(`Expected ${actual} to be truthy, but it was falsy.`);
       }
     },
     isFalsy: () => {
-      if (!actual) {
-        return {
-          pass: true,
-          message: `Expected ${actual} to be falsy`,
-        };
-      } else {
-        return {
-          pass: false,
-          message: `Expected ${actual} to be falsy, but it was truthy.`,
-        };
+      if (actual) {
+        throw new Error(`Expected ${actual} to be falsy, but it was truthy.`);
       }
     },
   };
